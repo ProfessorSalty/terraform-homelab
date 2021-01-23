49e03776149fe62d9ed9115567ff5e9dc7245a03
Greg Smith <me@gregsmith.nyc>
2021-01-23 12:16:25 -0500

    updates

--- main.tf
+++ main.tf
@@ -14,18 +14,20 @@ provider "proxmox" {
 }
 
 resource "proxmox_vm_qemu" "proxmox_resource" {
-  name                   = var.name
-  target_node            = var.target_node
-  vmid                   = var.vmid
-  clone                  = var.clone_source
-  cores                  = var.cores
-  memory                 = var.memory
-  os_type                = var.os_type
-  balloon                = 2048
-  onboot                 = true
-  full_clone             = false
-  boot                   = "c"
-  agent                  = 1
+  name        = var.name
+  target_node = var.target_node
+  vmid        = var.vmid
+  clone       = var.clone_source
+  cores       = var.cores
+  memory      = var.memory
+  os_type     = var.os_type
+  balloon     = 2048
+  onboot      = true
+  full_clone  = false
+  boot        = "c"
+  agent       = 1
+  hotplug     = "network,disk,cpu"
+  ipconfig0   = "ip=dhcp"
 
   dynamic "network" {
     for_each = var.networks
